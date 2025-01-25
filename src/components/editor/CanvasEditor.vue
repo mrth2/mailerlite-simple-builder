f4a068e574d18825955c376cc5f9023c80051df9
mrth2 <mr.th2.90@gmail.com>
2025-01-25 14:38:14 +0700

    update canvas editor to receive dropped block data

--- src/components/editor/CanvasEditor.vue
+++ src/components/editor/CanvasEditor.vue
@@ -1,10 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+function onDragBlockOver(event: DragEvent) {
+  event.preventDefault();
+  // console.log("Drag over", event.dataTransfer, event.dataTransfer?.dropEffect);
+}
+function onDropBlock(event: DragEvent) {
+  event.preventDefault();
+  console.log("Drop", event.dataTransfer, event.dataTransfer?.dropEffect);
+  try {
+    const blockData = JSON.parse(event.dataTransfer?.getData("block") || "{}");
+    console.log("Block data", blockData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+</script>
 
 <template>
   <section class="canvas-editor">
-    <div
-      class="bg-white rounded-lg p-8 max-w-screen-md mx-auto min-h-[calc(100vh-2rem)] shadow-sm"
-    >
+    <div class="canvas-area" @dragover="onDragBlockOver" @drop="onDropBlock">
       <h2 class="text-xl font-semibold mb-4">Editor</h2>
       <!-- Editor content will go here -->
     </div>
@@ -14,5 +27,9 @@
 <style scoped lang="postcss">
 .canvas-editor {
   @apply p-10;
+
+  .canvas-area {
+    @apply bg-white rounded-lg p-8 max-w-screen-md mx-auto min-h-[calc(100vh-2rem)] shadow-sm;
+  }
 }
 </style>

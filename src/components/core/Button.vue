2eab0e9987ef627a0f5cad4be634e981ef7db0d0
mrth2 <mr.th2.90@gmail.com>
2025-01-25 22:31:35 +0700

    add core button component

---
+++ src/components/core/Button.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { withDefaults, defineProps } from "vue";
+withDefaults(
+  defineProps<{
+    type?: "button" | "submit";
+  }>(),
+  {
+    type: "button",
+  }
+);
+</script>
+<template>
+  <button class="btn" type="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="postcss">
+.btn {
+  @apply rounded py-2 px-5 bg-accent text-white bg-opacity-100 hover:bg-opacity-60 transition-opacity duration-200;
+}
+</style>

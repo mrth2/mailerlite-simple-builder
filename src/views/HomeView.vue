144285be3406f9142dc25ed2b3f2442f038cd28e
mrth2 <mr.th2.90@gmail.com>
2025-01-26 00:40:30 +0700

    add empty div to fill gap in HomeView layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,7 @@ import HeaderMenu from "@/components/editor/HeaderMenu.vue";
   <main class="grid bg-neutral grid-cols-[minmax(250px,350px)_1fr] gap-4 pt-14">
     <HeaderMenu />
     <SideMenu />
+    <div /> <!-- Empty div to fill the gap -->
     <CanvasEditor />
   </main>
 </template>

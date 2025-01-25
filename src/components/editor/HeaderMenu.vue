9fd54ca75a469426c198b3d0494ff12a6d8fb4f3
mrth2 <mr.th2.90@gmail.com>
2025-01-25 22:44:59 +0700

    add HeaderMenu component with save btn to console the blocks data

---
+++ src/components/editor/HeaderMenu.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useEditorStore } from "@/stores";
+import Button from "../core/Button.vue";
+import { ref } from "vue";
+
+const EditorStore = useEditorStore();
+
+const isSaving = ref(false);
+function onSave() {
+  isSaving.value = true;
+  console.log("blocks data");
+  console.log(JSON.stringify(EditorStore.data, null, 2));
+  setTimeout(() => {
+    isSaving.value = false;
+  }, 1000);
+}
+</script>
+
+<template>
+  <header class="header-menu">
+    <div class="text-lg font-bold uppercase">Simple Builder</div>
+    <p class="text-xs mt-1.5 mr-auto">by HoaTrinh</p>
+    <Button @click="onSave">
+      {{ isSaving ? "Saving..." : "Save" }}
+    </Button>
+  </header>
+</template>
+
+<style scoped lang="postcss">
+.header-menu {
+  @apply absolute top-0 inset-x-0 bg-black text-white px-4 py-5 h-14;
+  @apply flex justify-between items-center gap-2;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import SideMenu from "@/components/editor/SideMenu.vue";
 import CanvasEditor from "@/components/editor/CanvasEditor.vue";
+import HeaderMenu from "@/components/editor/HeaderMenu.vue";
 </script>
 
 <template>
-  <main class="grid bg-neutral grid-cols-[minmax(250px,350px)_1fr] gap-4">
+  <main class="grid bg-neutral grid-cols-[minmax(250px,350px)_1fr] gap-4 py-14">
+    <HeaderMenu />
     <SideMenu />
     <CanvasEditor />
   </main>

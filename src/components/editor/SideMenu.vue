5c9f367188c14f49d7b7bd94ccef8578bc184632
mrth2 <mr.th2.90@gmail.com>
2025-01-25 14:37:57 +0700

    update side menu to initial text/image block data and store to drag event

--- src/components/editor/SideMenu.vue
+++ src/components/editor/SideMenu.vue
@@ -1,11 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useEditorStore } from "@/stores";
+import type { IEditorBlock } from "@/types";
+
+const EditorStore = useEditorStore();
+
+function onStartDragBlock(event: DragEvent, blockType: IEditorBlock["type"]) {
+  event.dataTransfer?.setData(
+    "block",
+    JSON.stringify(EditorStore.generateEmptyBlock(blockType))
+  );
+}
+</script>
 <template>
   <nav class="side-menu">
     <h2 class="text-xl font-semibold">Blocks</h2>
     <p>Simply drag & drop blocks below to the right</p>
     <ul class="ui-blocks">
-      <li class="mb-2">Text</li>
-      <li class="mb-2">Image</li>
+      <li draggable="true" @dragstart="onStartDragBlock($event, 'text')">
+        Text
+      </li>
+      <li draggable="true" @dragstart="onStartDragBlock($event, 'image')">
+        Image
+      </li>
     </ul>
   </nav>
 </template>
@@ -16,7 +32,7 @@
   ul.ui-blocks {
     @apply flex flex-col gap-3;
     li {
-      @apply bg-green-200 p-2 rounded-lg shadow-sm text-primary;
+      @apply bg-green-200 p-2 rounded-lg shadow-sm text-primary cursor-move;
     }
   }
 }
